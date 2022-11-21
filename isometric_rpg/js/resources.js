9d1ea662fd2ae0799d86b6fd28ffc6799dcd71a4
jumpjack <jumpjack@libero.it>
2022-11-21 15:14:25 +0100

    Update resources.js

--- isometric_rpg/js/resources.js
+++ isometric_rpg/js/resources.js
@@ -14,7 +14,7 @@ var resources = [
      * { name: "example01", type: "tmx", src: "data/map/example01.json" },
       */
     //{ name: "isometric", type: "tmx", src: "data/map/isometric.tmx" },
-    { name: "isometric", type: "tmx", src: "data/map/isometric-org.tmx" },
+    { name: "isometric", type: "tmx", src: "data/map/isometric.tmx" },
   //  { name: "isometric_staggered", type: "tmx", src: "data/map/isometric_staggered.tmx" }
 
     /* Background music.
