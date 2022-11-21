84f7cc61de7a24d0c05a6aec3a0fa1bf3b93f74a
jumpjack <jumpjack@libero.it>
2022-11-21 16:32:24 +0100

    Add files via upload

--- isometric_rpg/data/map/s1999tileset.tsx
+++ isometric_rpg/data/map/s1999tileset.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.9" tiledversion="1.9.2" name="single" tilewidth="24" tileheight="40" tilecount="64" columns="16">
- <image source="myimg/single.png" trans="898989" width="384" height="160"/>
+ <image source="../img/single.png" trans="898989" width="384" height="160"/>
 </tileset>
